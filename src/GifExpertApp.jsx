804a80000d5124724498aac714a4ca438a94df70
Álvaro Gutiérrez <a.gutierrez.ayestarain@gmail.com>
2022-07-16 18:56:42 +0200

    Añadido componente GifGrid y funcion getGifs

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
 import AddCategory from './components/AddCategory';
-
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']); //Asi siempre será un array
+    const [categories, setCategories] = useState(['One Punch']); //Asi siempre será un array
 
-    const onAddCategory = (inputValue) => {
+    const onAddCategory = (newCategory) => {
 
-        setCategories([inputValue, ...categories])
+        if (categories.includes(newCategory)) return; //Si esta ya la categoria, sale de la funcion
+
+        setCategories([newCategory, ...categories]) //En caso de que no esté, lo añade, junto con las categorias que ya están
 
         //setCategories(cat => [...cat, 'Valorant']);
 
@@ -21,13 +23,20 @@ export const GifExpertApp = () => {
     return (
         <>
             <h1>GifExpertApp</h1>
-            <AddCategory onNewCategory={onAddCategory} />
-            <button onClick={onAddCategory}>Agregar</button>
-            <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
+
+            <AddCategory
+                onNewCategory={(value) => onAddCategory(value)} />
+
+
+
+            {
+                categories.map((category) => ( //Un fragmento no puede tener el key
+                    <GifGrid
+                        key={category}
+                        category={category} />
+                ))
+            }
+
 
 
         </>
